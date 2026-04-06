8492339cd4f76cbc9d69644cb3d89326e34671cd
Ivan <ronya.sveta@gmail.com>
2026-04-06 19:03:12 +0300

    test commit

---
+++ GIFT_CLASS/GIFT_CLASS.cpp
@@ -0,0 +1,12 @@
+﻿// GIFT_CLASS.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::cout << "Test git class {step 1 }"<< endl;
+}
+
+
