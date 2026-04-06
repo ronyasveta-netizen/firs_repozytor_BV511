04b60a102c8d30a4101828d4bf082ea0a2076b2d
Ivan <ronya.sveta@gmail.com>
2026-04-06 20:20:48 +0300

    test 3

--- GIFT_CLASS/GIFT_CLASS.cpp
+++ GIFT_CLASS/GIFT_CLASS.cpp
@@ -8,6 +8,7 @@ int main()
     std::cout << "Hello World!\n";
     std::cout << "Test git class {step 1 }" << endl;
     cout << "Test git class {step 2 }" << endl;
+    cout << "Test git class {step 3 }" << endl;
 }
 
 
